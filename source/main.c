5972cbdb9962de102f48e550e0c0f83ba48a856e
tganeshg <tganeshg8@gmail.com>
2024-12-14 20:11:27 +0530

    Clean up main.c

--- source/main.c
+++ source/main.c
@@ -8,17 +8,14 @@
 #include "lvgl/lvgl.h"
 #include "lvgl/demos/lv_demos.h"
 
-#if LV_USE_WAYLAND
-#include "backends/interface.h"
-#endif
+#define LV_LINUX_EVDEV_POINTER_DEVICE       "/dev/input/event0"
+#define LV_SIM_WINDOW_WIDTH                 800
+#define LV_SIM_WINDOW_HEIGHT                480
 
-uint16_t window_width;
-uint16_t window_height;
-bool fullscreen;
-bool maximize;
+uint16_t window_width = LV_SIM_WINDOW_WIDTH;
+uint16_t window_height = LV_SIM_WINDOW_HEIGHT;
 
-static void configure_simulator(int argc, char **argv);
-
+/*** Private ***/
 static const char *getenv_default(const char *name, const char *dflt)
 {
     return getenv(name) ? : dflt;
@@ -31,7 +28,7 @@ static void lv_linux_init_input_pointer(lv_display_t *disp)
      * Use 'evtest' to find the correct input device. /dev/input/by-id/ is recommended if possible
      * Use /dev/input/by-id/my-mouse-or-touchscreen or /dev/input/eventX
      */
-    const char *input_device = getenv("LV_LINUX_EVDEV_POINTER_DEVICE");
+    const char *input_device = LV_LINUX_EVDEV_POINTER_DEVICE;
 
     if (input_device == NULL) {
         fprintf(stderr, "please set the LV_LINUX_EVDEV_POINTER_DEVICE environment variable\n");
@@ -40,12 +37,6 @@ static void lv_linux_init_input_pointer(lv_display_t *disp)
 
     lv_indev_t *touch = lv_evdev_create(LV_INDEV_TYPE_POINTER, input_device);
     lv_indev_set_display(touch, disp);
-
-    /* Set the cursor icon */
-    LV_IMAGE_DECLARE(mouse_cursor_icon);
-    lv_obj_t * cursor_obj = lv_image_create(lv_screen_active());
-    lv_image_set_src(cursor_obj, &mouse_cursor_icon);
-    lv_indev_set_cursor(touch, cursor_obj);
 }
 #endif
 
@@ -61,32 +52,10 @@ static void lv_linux_disp_init(void)
 
     lv_linux_fbdev_set_file(disp, device);
 }
-#elif LV_USE_LINUX_DRM
-static void lv_linux_disp_init(void)
-{
-    const char *device = getenv_default("LV_LINUX_DRM_CARD", "/dev/dri/card0");
-    lv_display_t * disp = lv_linux_drm_create();
-
-#if LV_USE_EVDEV
-    lv_linux_init_input_pointer(disp);
-#endif
-
-    lv_linux_drm_set_file(disp, device, -1);
-}
-#elif LV_USE_SDL
-static void lv_linux_disp_init(void)
-{
-
-    lv_sdl_window_create(window_width, window_height);
-
-}
-#elif LV_USE_WAYLAND
-    /* see backend/wayland.c */
 #else
-#error Unsupported configuration
+    #error Unsupported configuration
 #endif
 
-#if LV_USE_WAYLAND == 0
 void lv_linux_run_loop(void)
 {
     uint32_t idle_time;
@@ -98,59 +67,10 @@ void lv_linux_run_loop(void)
         usleep(idle_time * 1000);
     }
 }
-#endif
 
-/*
- * Process command line arguments and environment
- * variables to configure the simulator
- */
-static void configure_simulator(int argc, char **argv)
-{
-    int opt = 0;
-
-    /* Default values */
-    fullscreen = maximize = false;
-    window_width = atoi(getenv("LV_SIM_WINDOW_WIDTH") ? : "800");
-    window_height = atoi(getenv("LV_SIM_WINDOW_HEIGHT") ? : "480");
-
-    /* Parse the command-line options. */
-    while ((opt = getopt (argc, argv, "fmw:h:")) != -1) {
-        switch (opt) {
-        case 'f':
-            fullscreen = true;
-            if (LV_USE_WAYLAND == 0) {
-                fprintf(stderr, "The SDL driver doesn't support fullscreen mode on start\n");
-                exit(1);
-            }
-            break;
-        case 'm':
-            maximize = true;
-            if (LV_USE_WAYLAND == 0) {
-                fprintf(stderr, "The SDL driver doesn't support maximized mode on start\n");
-                exit(1);
-            }
-            break;
-        case 'w':
-            window_width = atoi(optarg);
-            break;
-        case 'h':
-            window_height = atoi(optarg);
-            break;
-        case ':':
-            fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-            exit(1);
-        case '?':
-            fprintf (stderr, "Unknown option -%c.\n", optopt);
-            exit(1);
-        }
-    }
-}
-
+/* Main */
 int main(int argc, char **argv)
 {
-
-    configure_simulator(argc, argv);
-
     /* Initialize LVGL. */
     lv_init();
 
